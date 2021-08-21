8fee56b8b6a1c67f32a3a3e5588c4ce7b9863ca4
“Hannnk” <megamhc@gmail.com>
2021-08-21 22:36:43 +0800

    new

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#-*- coding:utf-8 -*-
+import tornado.web
+import tornado.ioloop
 
 
-# Press the green button in the gutter to run the script.
+#定义处理类型
+class IndexHandler(tornado.web.RequestHandler):
+    #添加一个处理get请求方式的方法
+    def get(self):
+        #向响应中，添加数据
+        self.write('好看的皮囊千篇一律，有趣的灵魂万里挑一。')
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #创建一个应用对象
+    app = tornado.web.Application([(r'/',IndexHandler)])
+    #绑定一个监听端口
+    app.listen(8888)
+    #启动web程序，开始监听端口的连接
+    tornado.ioloop.IOLoop.current().start()
